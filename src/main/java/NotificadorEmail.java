8297634456166492392775510d91e8bb5f3d9b84
Extends-Object <return.unstring@gmail.com>
2025-04-12 02:58:22 -0300

    Correcciones extra

--- src/main/java/NotificadorEmail.java
+++ src/main/java/NotificadorEmail.java
@@ -19,12 +19,12 @@ public class NotificadorEmail implements Notificador{
     public void notificar(String emisor, String destinatario, String asunto, String cuerpo) {
 
 
-        // configure SMTP details                   //Las propiedades no se tocan son asi por defecto
+        // configure SMTP details                                   //Las propiedades no se tocan son asi por defecto
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "false");        // Desactivando startTLS porque no erconoce el certificado del servidor de Mailtrap cuando intenta usar startTLS
+        props.put("mail.smtp.starttls.enable", "false");            // Desactivando startTLS porque no erconoce el certificado del servidor de Mailtrap cuando intenta usar startTLS
         props.put("mail.smtp.host", host);
-        props.put("mail.smtp.port", "25");          //Cambiando 587 por 25 --> puerto sin TLS
+        props.put("mail.smtp.port", "25");                          //Cambiando 587 por 25 --> puerto sin TLS
 
         // create the mail Session object
         Session session = Session.getInstance(props,                //Hace una instancia de la sesion con las prop config
@@ -37,17 +37,17 @@ public class NotificadorEmail implements Notificador{
 
         try {
 
-            Message message = new MimeMessage(session);                 //Contenedor del mail que tiene todas las cosas del mail
-            message.setFrom(new InternetAddress(emisor));                 //Crea un objeto dir de internet que recibe el mail
-            message.setRecipient(Message.RecipientType.TO, new InternetAddress(destinatario));            //Es quien soy
-            message.setSubject(asunto);                                       //Asunto
-            message.setText(cuerpo);                                                           //Cuerpo del mail
+            Message message = new MimeMessage(session);                                  //Contenedor del mail que tiene todas las cosas del mail
+            message.setFrom(new InternetAddress(emisor));                                //Crea un objeto dir de internet que recibe el mail
+            message.setRecipient(Message.RecipientType.TO, new InternetAddress(destinatario));      //Es quien soy
+            message.setSubject(asunto);                                                             //Asunto
+            message.setText(cuerpo);                                                                //Cuerpo del mail
 
 
-            Transport.send(message);                //Envia el mail
+            Transport.send(message);                                    //Envia el mail
             System.out.println("El mensaje se envió correctamente");
 
-        } catch (MessagingException e) {                //Por si falla el envio
+        } catch (MessagingException e) {                                //Por si falla el envio
             throw new RuntimeException(e);
         }
     }
